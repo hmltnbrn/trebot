43a6def8a3b3c0726b1f3d9b459b0a33669a861e
Brian Hamilton <hmltnbrn@gmail.com>
2019-07-29 16:03:28 -0400

    Use display name instead of username

--- bot/index.js
+++ bot/index.js
@@ -38,7 +38,7 @@ client.on('message', asyncWrap(async msg => {
   // Get an answer
   else if (args[0] === 'answer' || args[0] === 'a') {
     if(args[1]) { // If a user supplied an answer
-      var { log, reset } = await commands.contestantAnswer(msg.channel, randomAnswer[msg.channel.id], msg.author.username, args.slice(1).join(' '));
+      var { log, reset } = await commands.contestantAnswer(msg.channel, randomAnswer[msg.channel.id], msg.member.displayName, args.slice(1).join(' '));
       if(reset) randomAnswer[msg.channel.id] = "";
     }
     else {
