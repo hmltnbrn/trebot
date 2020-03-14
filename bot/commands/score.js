f4da87586e8458a65f9677885782d3550602b53d
Brian Hamilton <hmltnbrn@gmail.com>
2020-03-14 13:26:51 -0400

    Fix 0 score

--- bot/commands/score.js
+++ bot/commands/score.js
@@ -7,7 +7,7 @@ module.exports = async (channel, guild) => {
     return Promise.reject(e);
   }
   let scoreText = contestants.map(c => {
-    return `${c.user_tag} -- $${c.score} (${c.correct_answers} correct|${c.incorrect_answers} incorrect)`;
+    return `${c.user_tag} -- $${c.score} (${c.correct_answers || 0} correct|${c.incorrect_answers || 0} incorrect)`;
   });
   channel.send(contestants.length > 0 ? scoreText.join('\n') : "No one's played yet, honey.");
   return Promise.resolve("Responding with score");
