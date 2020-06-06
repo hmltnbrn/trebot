7f2ce4ac10d411b2f7bf0c2ea2aa5b085997a675
Brian Hamilton <hmltnbrn@gmail.com>
2020-06-06 13:16:12 -0400

    Percentage slice

--- bot/commands/score.js
+++ bot/commands/score.js
@@ -9,10 +9,10 @@ module.exports = async (channel, guild) => {
   let scoreText = contestants.map(c => {
     const correct = +c.correct_answers || 0;
     const incorrect = +c.incorrect_answers || 0;
-    const perc = (correct/(correct+incorrect)).toFixed(3).slice(1);
+    const perc = (correct/(correct+incorrect)).toFixed(3);
     return {
       name: c.user_tag,
-      value: `$${c.score.toLocaleString()} (${correct}-${incorrect} ${perc})`
+      value: `$${c.score.toLocaleString()} (${correct}-${incorrect} ${perc[0] === '1' ? perc : perc.slice(1)})`
     }
   });
   channel.send(contestants.length === 0 ? "No one's played yet, honey." : {embed: {
