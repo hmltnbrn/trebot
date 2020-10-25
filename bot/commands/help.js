02f8044b2ea31b6ecf80af9e5f387324a5fa2b60
Brian Hamilton <hmltnbrn@gmail.com>
2020-10-25 14:17:33 -0400

    Move help to embed

--- bot/commands/help.js
+++ bot/commands/help.js
@@ -1,12 +1,32 @@
-const helpText = `Welcome to Trebot! I can help you play Jeopardy.
-
-${process.env.BOT_PREFIX || '.'}trebot [q]uestion [20|30] ............ Question with optional 20 or 30 second timer
-${process.env.BOT_PREFIX || '.'}trebot [c]lue ..................................... Option to show up to three different clues (will remove 25% for each clue from the winning value)
-${process.env.BOT_PREFIX || '.'}trebot [a]nswer <answer text> ... Try to answer previous non-timed question (will reveal answer and increase the score for the contestant if answered correctly/decrease if answered incorrectly)
-${process.env.BOT_PREFIX || '.'}trebot [a]nswer ............................... Answer to previously asked question
-${process.env.BOT_PREFIX || '.'}trebot [s]core .................................. Get the score of the server's game`;
+const prefix = process.env.BOT_PREFIX || '.';
+const helpText = [
+  {
+    name: `${prefix}trebot [q]uestion <20|30>`,
+    value: 'Question with optional 20 or 30 second timer'
+  },
+  {
+    name: `${prefix}trebot [c]lue`,
+    value: 'Option to show up to three different clues (will remove 25% for each clue from the winning value)'
+  },
+  {
+    name: `${prefix}trebot [a]nswer <answer text>`,
+    value: 'Try to answer previous non-timed question (will reveal answer and increase the score for the contestant if answered correctly/decrease if answered incorrectly)'
+  },
+  {
+    name: `${prefix}trebot [a]nswer`,
+    value: 'Answer to previously asked question'
+  },
+  {
+    name: `${prefix}trebot [s]core`,
+    value: 'Get the score of the server\'s game'
+  }
+];
 
 module.exports = async channel => {
-  channel.send(helpText);
+  channel.send({embed: {
+    color: 0x060CE9,
+    title: "Help",
+    fields: helpText
+  }});
   return Promise.resolve("Responding with help");
 }
