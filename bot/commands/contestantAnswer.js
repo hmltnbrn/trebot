e0e21ca2d90bcacb556eb8aef0e6ff2e8a4403aa
Brian Hamilton <hmltnbrn@gmail.com>
2022-06-15 19:08:53 -0400

    Modify answering

--- bot/commands/contestantAnswer.js
+++ bot/commands/contestantAnswer.js
@@ -6,50 +6,51 @@ const db = require('../../db');
 
 module.exports = async (channel, guild, answer, value, member, contestantAnswer, clueAmount) => {
   try {
-    if(answer) {
-      const turndownService = new TurndownService()
-      const markdown = turndownService.turndown(answer); // Convert HTML to markdown
-      if(contestantAnswer) {
-        const stringAnswer = removeMd(markdown).toLowerCase();
-        const similarity = stringSimilarity.compareTwoStrings(stringAnswer, contestantAnswer);
-        if(similarity >= 0.6 || checkPartial(stringAnswer.split(' '), contestantAnswer.split(' '))) { // Check for a 60% similarity rating between the contestant's answer and the actual answer OR some words being present
-          const valuePerc = 0.25*(4-clueAmount);
-          const givenValue = clueAmount > 0 ? value*valuePerc : value;
-          const contestant = await db.increaseContestantScore(guild.id, member, givenValue);
-          channel.send({embed: {
-            color: 0x4caf50,
-            title: `${member.displayName} is correct!`,
-            description: `The answer is ${markdown}`,
-            fields: [{
-              name: "-----------",
-              value: `${member.displayName}'s cash winnings are now $${contestant.score.toLocaleString()}`
-            }],
-            footer: {
-              text: `Awarded $${givenValue.toLocaleString()} for the correct answer`
-            },
-          }});
-          return Promise.resolve({ log: "Responding with answer", reset: true });
-        }
-        const contestant = await db.decreaseContestantScore(guild.id, member, value);
-        channel.send({embed: {
-          color: 0xf44336,
-          title: `Sorry, ${member.displayName}, that's incorrect.`,
-          description: `${member.displayName}'s cash winnings are now $${contestant.score.toLocaleString()}`,
-          footer: {
-            text: `Took away $${value.toLocaleString()} for the incorrect answer`
-          },
-        }});
-      }
-      else {
-        channel.send("There is a mistake, honey.");
-        return Promise.resolve({ log: "No answer provided", reset: false });
-      }
-      return Promise.resolve({ log: "Responding with correctness", reset: false });
-    }
-    else {
+    if(!answer) {
       channel.send("You need a question first, honey.");
       return Promise.resolve({ log: "No saved question", reset: false });
     }
+    if(!contestantAnswer) {
+      channel.send("There is a mistake, honey.");
+      return Promise.resolve({ log: "No answer provided", reset: false });
+    }
+
+    const turndownService = new TurndownService()
+    const markdown = turndownService.turndown(answer); // Convert HTML to markdown
+    const stringAnswer = removeMd(markdown).toLowerCase();
+    const similarity = stringSimilarity.compareTwoStrings(stringAnswer, contestantAnswer);
+
+    if(similarity >= 0.6 || checkPartial(stringAnswer.split(' '), contestantAnswer.split(' '))) { // Check for a 60% similarity rating between the contestant's answer and the actual answer OR some words being present
+      const valuePerc = 0.25*(4-clueAmount);
+      const givenValue = clueAmount > 0 ? value*valuePerc : value;
+      const contestant = await db.increaseContestantScore(guild.id, member, givenValue);
+      channel.send({embed: {
+        color: 0x4caf50,
+        title: `${member.displayName} is correct!`,
+        description: `The answer is ${markdown}`,
+        fields: [{
+          name: "-----------",
+          value: `${member.displayName}'s cash winnings are now $${contestant.score.toLocaleString()}`
+        }],
+        footer: {
+          text: `Awarded $${givenValue.toLocaleString()} for the correct answer`
+        },
+      }});
+      return Promise.resolve({ log: "Responding with answer", reset: true });
+    }
+
+    const contestant = await db.decreaseContestantScore(guild.id, member, value);
+
+    channel.send({embed: {
+      color: 0xf44336,
+      title: `Sorry, ${member.displayName}, that's incorrect.`,
+      description: `${member.displayName}'s cash winnings are now $${contestant.score.toLocaleString()}`,
+      footer: {
+        text: `Took away $${value.toLocaleString()} for the incorrect answer`
+      },
+    }});
+
+    return Promise.resolve({ log: "Responding with correctness", reset: false });
   } catch (e) {
     return Promise.reject(e);
   }
